7f241e33eecfb62d62adfeb036431b556bd22130
João Victor <jvsb21@gmail.com>
2019-02-24 21:48:09 -0300

    Adição de funcionalidades na tela de cadastro dos jogos

--- app/src/main/java/com/game/next/nextgame/ActivityIdentificaJogo.java
+++ app/src/main/java/com/game/next/nextgame/ActivityIdentificaJogo.java
@@ -2,16 +2,21 @@ package com.game.next.nextgame;
 
 import android.content.Intent;
 import android.graphics.Color;
+import android.graphics.PorterDuff;
 import android.graphics.drawable.Drawable;
 import android.support.annotation.DrawableRes;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.SeekBar;
 import android.widget.TextView;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
 import com.game.next.nextgame.adapters.MyAdapter;
@@ -34,11 +39,18 @@ public class ActivityIdentificaJogo extends AppCompatActivity {
 
     private TextView txtCodIdentificado, txtCodigoDeBarras, txtPrecoVendaJogo, txtPrecoAluguelJogo;
     private ImageView imgCodIdentificado;
-    private Button btnAdicionarJogoManualmente, btnAdicionarJogoSucesso;
+    private Button btnAdicionarJogoManualmente, btnAdicionarJogoSucesso, btnAdicionarHorarioIdentificaJogo;
     private ArrayList<Jogo> jogosPS4 = new ArrayList<>();
     private ArrayList<Jogo> jogosXbox = new ArrayList<>();
 
+    private LinearLayout laySeekAlugar, laySeekPreco;
+    private LinearLayout layHorarios1, layHorarios2, layHorarios3;
+    private CheckBox checkHora1, checkHora2, checkHora3;
+    private TextView txtHora1, txtHora2, txtHora3, txtExcluirHora1, txtExcluirHora2, txtExcluirHora3;
+
     private Button btnDomingo, btnSegunda, btnTerca, btnQuarta, btnQuinta, btnSexta, btnSabado;
+
+    private Button btnAlugar, btnAlugarEVender, btnVender;
 
     private boolean domingoSelecionado = false;
     private boolean segundaSelecionado = false;
@@ -48,6 +60,8 @@ public class ActivityIdentificaJogo extends AppCompatActivity {
     private boolean sextaSelecionado = false;
     private boolean sabadoSelecionado = false;
 
+    private TimePicker horarioIdentificaJogo;
+    private TimePicker horarioIdentificaJogo2;
 
     private Jogo jogoIdentificado = new Jogo();
 
@@ -58,6 +72,14 @@ public class ActivityIdentificaJogo extends AppCompatActivity {
 
     private int progressChangedValuePreco = 0;
     private int progressChangedValueAluguel = 0;
+
+    private int hora = 9;
+    private int minuto = 30;
+
+    private int hora2 = 10;
+    private int minuto2 = 30;
+
+    private int quantidadeMaxDeHorarios = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -80,7 +102,146 @@ public class ActivityIdentificaJogo extends AppCompatActivity {
         btnQuinta = (Button) findViewById(R.id.btn_quinta);
         btnSexta = (Button) findViewById(R.id.btn_sexta);
         btnSabado = (Button) findViewById(R.id.btn_sabado);
+        horarioIdentificaJogo = (TimePicker)findViewById(R.id.horario_identifica_jogo);
+        horarioIdentificaJogo2 = (TimePicker)findViewById(R.id.horario_2_identifica_jogo);
 
+        laySeekAlugar = (LinearLayout) findViewById(R.id.lay_seek_aluguel);
+        laySeekPreco = (LinearLayout) findViewById(R.id.lay_seek_preco);
+        btnAlugar = (Button) findViewById(R.id.btn_alugar_identifica_jogo);
+        btnAlugarEVender = (Button) findViewById(R.id.btn_alugar_e_vender_identifica_jogo);
+        btnVender = (Button) findViewById(R.id.btn_vender_identifica_jogo);
+
+        btnAdicionarHorarioIdentificaJogo = (Button) findViewById(R.id.btn_adicionar_horario_identifica_jogo);
+        layHorarios1 = (LinearLayout) findViewById(R.id.lay_horarios_1);
+        layHorarios2 = (LinearLayout) findViewById(R.id.lay_horarios_2);
+        layHorarios3 = (LinearLayout) findViewById(R.id.lay_horarios_3);
+        checkHora1 = (CheckBox) findViewById(R.id.check_hora1_identifica_jogo);
+        checkHora2 = (CheckBox) findViewById(R.id.check_hora2_identifica_jogo);
+        checkHora3 = (CheckBox) findViewById(R.id.check_hora3_identifica_jogo);
+        txtHora1 = (TextView) findViewById(R.id.txt_hora1_identifica_jogo);
+        txtHora2 = (TextView) findViewById(R.id.txt_hora2_identifica_jogo);
+        txtHora3 = (TextView) findViewById(R.id.txt_hora3_identifica_jogo);
+        txtExcluirHora1 = (TextView) findViewById(R.id.txt_excluir_hora1_identifica_jogo);
+        txtExcluirHora2 = (TextView) findViewById(R.id.txt_excluir_hora2_identifica_jogo);
+        txtExcluirHora3 = (TextView) findViewById(R.id.txt_excluir_hora3_identifica_jogo);
+
+        btnAlugar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                laySeekAlugar.setVisibility(View.VISIBLE);
+                laySeekPreco.setVisibility(View.GONE);
+
+                btnAlugar.getBackground().setColorFilter(ContextCompat.getColor(ActivityIdentificaJogo.this, R.color.corRoxo), PorterDuff.Mode.MULTIPLY);
+                btnAlugarEVender.getBackground().setColorFilter(ContextCompat.getColor(ActivityIdentificaJogo.this, R.color.corAzul), PorterDuff.Mode.MULTIPLY);
+                btnVender.getBackground().setColorFilter(ContextCompat.getColor(ActivityIdentificaJogo.this, R.color.corAzul), PorterDuff.Mode.MULTIPLY);
+
+            }
+        });
+
+        btnAlugarEVender.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                laySeekAlugar.setVisibility(View.VISIBLE);
+                laySeekPreco.setVisibility(View.VISIBLE);
+
+                btnAlugar.getBackground().setColorFilter(ContextCompat.getColor(ActivityIdentificaJogo.this, R.color.corAzul), PorterDuff.Mode.MULTIPLY);
+                btnAlugarEVender.getBackground().setColorFilter(ContextCompat.getColor(ActivityIdentificaJogo.this, R.color.corRoxo), PorterDuff.Mode.MULTIPLY);
+                btnVender.getBackground().setColorFilter(ContextCompat.getColor(ActivityIdentificaJogo.this, R.color.corAzul), PorterDuff.Mode.MULTIPLY);
+            }
+        });
+
+        btnVender.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                laySeekAlugar.setVisibility(View.GONE);
+                laySeekPreco.setVisibility(View.VISIBLE);
+
+                btnAlugar.getBackground().setColorFilter(ContextCompat.getColor(ActivityIdentificaJogo.this, R.color.corAzul), PorterDuff.Mode.MULTIPLY);
+                btnAlugarEVender.getBackground().setColorFilter(ContextCompat.getColor(ActivityIdentificaJogo.this, R.color.corAzul), PorterDuff.Mode.MULTIPLY);
+                btnVender.getBackground().setColorFilter(ContextCompat.getColor(ActivityIdentificaJogo.this, R.color.corRoxo), PorterDuff.Mode.MULTIPLY);
+            }
+        });
+
+        horarioIdentificaJogo.setIs24HourView(true);
+
+        horarioIdentificaJogo.setCurrentHour(hora);
+        horarioIdentificaJogo.setCurrentMinute(minuto);
+
+        horarioIdentificaJogo.setOnTimeChangedListener(new TimePicker.OnTimeChangedListener() {
+            @Override
+            public void onTimeChanged(TimePicker view, int hourOfDay, int minute) {
+                hora = hourOfDay;
+                minuto = minute;
+            }
+        });
+
+        horarioIdentificaJogo2.setIs24HourView(true);
+
+        horarioIdentificaJogo2.setCurrentHour(hora2);
+        horarioIdentificaJogo2.setCurrentMinute(minuto2);
+
+        horarioIdentificaJogo2.setOnTimeChangedListener(new TimePicker.OnTimeChangedListener() {
+            @Override
+            public void onTimeChanged(TimePicker view, int hourOfDay, int minute) {
+                hora2 = hourOfDay;
+                minuto2 = minute;
+            }
+        });
+
+        btnAdicionarHorarioIdentificaJogo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(quantidadeMaxDeHorarios == 3){
+                    Toast.makeText(ActivityIdentificaJogo.this, "A quantidade máxima de horários são 3. Exclua 1 horário se quiser adicionar outro!",Toast.LENGTH_LONG).show();
+                }else{
+                    if(layHorarios1.getVisibility() == View.GONE){
+                        layHorarios1.setVisibility(View.VISIBLE);
+                        txtHora1.setText(hora + ":" + minuto + " - " + hora2 + ":" + minuto2);
+                        checkHora1.setChecked(true);
+                        quantidadeMaxDeHorarios++;
+
+                    }else if((layHorarios2.getVisibility() == View.GONE) && (layHorarios1.getVisibility() == View.VISIBLE)){
+                        layHorarios2.setVisibility(View.VISIBLE);
+                        txtHora2.setText(hora + ":" + minuto + " - " + hora2 + ":" + minuto2);
+                        checkHora2.setChecked(true);
+                        quantidadeMaxDeHorarios++;
+
+                    }else if((layHorarios3.getVisibility() == View.GONE) && (layHorarios2.getVisibility() == View.VISIBLE) && (layHorarios1.getVisibility() == View.VISIBLE)){
+                        layHorarios3.setVisibility(View.VISIBLE);
+                        txtHora3.setText(hora + ":" + minuto + " - " + hora2 + ":" + minuto2);
+                        checkHora3.setChecked(true);
+                        quantidadeMaxDeHorarios++;
+
+                    }
+                }
+            }
+        });
+
+        txtExcluirHora1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                layHorarios1.setVisibility(View.GONE);
+                quantidadeMaxDeHorarios--;
+            }
+        });
+
+        txtExcluirHora2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                layHorarios2.setVisibility(View.GONE);
+                quantidadeMaxDeHorarios--;
+            }
+        });
+
+        txtExcluirHora3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                layHorarios3.setVisibility(View.GONE);
+                quantidadeMaxDeHorarios--;
+            }
+        });
+
+        //Jogar no Firebase só os horários que estiverem visiveis na tela e checados! E só adicionar se tiver pelo menos 1 checado!
 
         btnDomingo.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -88,6 +249,7 @@ public class ActivityIdentificaJogo extends AppCompatActivity {
             // TODO Auto-generated method stub
                 if(domingoSelecionado == false) {
                     btnDomingo.setBackground(getResources().getDrawable(R.drawable.round_button_roxo));
+                    Toast.makeText(ActivityIdentificaJogo.this,"Domingo Adicionado!",Toast.LENGTH_LONG).show();
                     domingoSelecionado = true;
                 }else{
                     btnDomingo.setBackground(getResources().getDrawable(R.drawable.round_button_azul));
@@ -102,6 +264,7 @@ public class ActivityIdentificaJogo extends AppCompatActivity {
                 // TODO Auto-generated method stub
                 if(segundaSelecionado == false) {
                     btnSegunda.setBackground(getResources().getDrawable(R.drawable.round_button_roxo));
+                    Toast.makeText(ActivityIdentificaJogo.this,"Segunda-feira Adicionado!",Toast.LENGTH_LONG).show();
                     segundaSelecionado = true;
                 }else{
                     btnSegunda.setBackground(getResources().getDrawable(R.drawable.round_button_azul));
@@ -116,6 +279,7 @@ public class ActivityIdentificaJogo extends AppCompatActivity {
                 // TODO Auto-generated method stub
                 if(tercaSelecionado == false) {
                     btnTerca.setBackground(getResources().getDrawable(R.drawable.round_button_roxo));
+                    Toast.makeText(ActivityIdentificaJogo.this,"Terça-feira Adicionado!",Toast.LENGTH_LONG).show();
                     tercaSelecionado = true;
                 }else{
                     btnTerca.setBackground(getResources().getDrawable(R.drawable.round_button_azul));
@@ -130,6 +294,7 @@ public class ActivityIdentificaJogo extends AppCompatActivity {
                 // TODO Auto-generated method stub
                 if(quartaSelecionado == false) {
                     btnQuarta.setBackground(getResources().getDrawable(R.drawable.round_button_roxo));
+                    Toast.makeText(ActivityIdentificaJogo.this,"Quarta-feira Adicionado!",Toast.LENGTH_LONG).show();
                     quartaSelecionado = true;
                 }else{
                     btnQuarta.setBackground(getResources().getDrawable(R.drawable.round_button_azul));
@@ -144,6 +309,7 @@ public class ActivityIdentificaJogo extends AppCompatActivity {
                 // TODO Auto-generated method stub
                 if(quintaSelecionado == false) {
                     btnQuinta.setBackground(getResources().getDrawable(R.drawable.round_button_roxo));
+                    Toast.makeText(ActivityIdentificaJogo.this,"Quinta-feira Adicionado!",Toast.LENGTH_LONG).show();
                     quintaSelecionado = true;
                 }else{
                     btnQuinta.setBackground(getResources().getDrawable(R.drawable.round_button_azul));
@@ -158,6 +324,7 @@ public class ActivityIdentificaJogo extends AppCompatActivity {
                 // TODO Auto-generated method stub
                 if(sextaSelecionado == false) {
                     btnSexta.setBackground(getResources().getDrawable(R.drawable.round_button_roxo));
+                    Toast.makeText(ActivityIdentificaJogo.this,"Sexta-feira Adicionado!",Toast.LENGTH_LONG).show();
                     sextaSelecionado = true;
                 }else{
                     btnSexta.setBackground(getResources().getDrawable(R.drawable.round_button_azul));
@@ -172,6 +339,7 @@ public class ActivityIdentificaJogo extends AppCompatActivity {
                 // TODO Auto-generated method stub
                 if(sabadoSelecionado == false) {
                     btnSabado.setBackground(getResources().getDrawable(R.drawable.round_button_roxo));
+                    Toast.makeText(ActivityIdentificaJogo.this,"Sábado Adicionado!",Toast.LENGTH_LONG).show();
                     sabadoSelecionado = true;
                 }else{
                     btnSabado.setBackground(getResources().getDrawable(R.drawable.round_button_azul));
